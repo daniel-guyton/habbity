c3636e20eec39b4a2d80f6c349acad5c455583b4
Daniel <daniel.j.guyton@gmail.com>
2022-05-05 11:59:55 +1200

    Add checkbox disable on click

--- client/components/IndividualHabit.jsx
+++ client/components/IndividualHabit.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Box, Flex, Text, Checkbox, useColorModeValue, Button, } from '@chakra-ui/react'
 
 const IndividualHabit = (props) => {
   const primaryBgColor = useColorModeValue('gray.200', 'gray.700')
+  let [dayCount, setDayCount] = useState(0)
+  const handleCheckBoxClick = (e) => {
+    console.log(e.target)
+    setDayCount(dayCount += 1)
+    e.target.disabled = true
+  }
+  
+  console.log(dayCount)
+
+
   return (
       <Flex alignItems="center" mt="4" mb="4" bg={primaryBgColor} borderRadius="2">
       <Box
@@ -14,11 +24,11 @@ const IndividualHabit = (props) => {
         p='3'
         fontSize='16'
       >
-        <Checkbox pr="3" colorScheme="green" border='gray' defaultunchecked='true'/>
+        <Checkbox pr="3" colorScheme="green" border='gray' defaultunchecked='true' onChange={handleCheckBoxClick} isDisabled={false}/>
 
         <Text pl="3">{props.goal}</Text>
       </Box>
-      <Text p="3" whiteSpace="nowrap">7/28 Days</Text>
+      <Text p="3" whiteSpace="nowrap">{dayCount}/28 Days</Text>
       </Flex>
   )
 }
