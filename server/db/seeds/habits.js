4e5555498e1d2c66a86c29b1940bff56d7e363c5
Daniel <daniel.j.guyton@gmail.com>
2022-05-04 19:23:34 +1200

    convert names in seeds to strings

--- server/db/seeds/habits.js
+++ server/db/seeds/habits.js
@@ -6,9 +6,9 @@
 exports.seed = async function (knex) {
   await knex('habits').del()
   await knex('habits').insert([
-    { id: 10002, name: bob, points: 420 },
-    { id: 20002, name: bobby, points: 1337 },
-    { id: 40002, name: bobina, points: 80085 },
-    { id: 80002, name: bobtina, points: 69 },
+    { id: 10002, name: 'bob', points: 420 },
+    { id: 20002, name: 'bobby', points: 1337 },
+    { id: 40002, name: 'bobina', points: 80085 },
+    { id: 80002, name: 'bobtina', points: 69 },
   ])
 }
