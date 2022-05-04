c460cfc5a4d23bc085adcb750d077eda5d5ac03b
Daniel <daniel.j.guyton@gmail.com>
2022-05-05 09:26:02 +1200

    Add habit dashboard

--- client/components/Habits.jsx
+++ client/components/Habits.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import {
-  Heading,
   Box,
-  Input,
-  Button,
   Flex,
+  Text,
+  useColorModeValue
 } from '@chakra-ui/react'
 import {AddIcon } from '@chakra-ui/icons'
 import IndividualHabit from './IndividualHabit'
-import AchievedHabits from './AchievedHabits'
-const Habits = () => {
-
+// import AchievedHabits from './AchievedHabits'
+const Habits = () => {  
+  const primaryBgColor = useColorModeValue('gray.100', 'gray.800')
+  const primaryFontColor = useColorModeValue('#333', 'white')
   const goals = [{
     goal: 'Go to sleep'
   }, {
@@ -18,21 +18,81 @@ const Habits = () => {
   }]
 
   return (
-    <Box maxW="650">
-      <Heading>Habits</Heading>
-      <Box maxW="650" mt="5" mb="5">
-        <Flex height={50}>
-          <Input placeholder="Add new task"></Input>
-          <Button ml="2" colorScheme="green">
-            <AddIcon w={3} h={3} />
-          </Button>
-        </Flex>
+    <Flex width="100%" flexWrap="wrap" color={primaryFontColor}>
+      <Box
+        flexGrow={1}
+        maxW="420"
+        minW="420"
+        height="600"
+        borderColor="gray.500"
+        boxShadow="md"
+        bg={primaryBgColor}
+        p="6"
+        m="3"
+        position="relative"
+        borderTop="4px"
+        borderTopColor="green.300"
+        
+      >
+        <Text pb="1" as="span" borderColor="green.200" fontWeight={500} fontSize="18px">
+          In Progress
+        </Text>
+
+        {goals.map(({ goal }, idx) => {
+          return <IndividualHabit key={idx} goal={goal} />
+        })}
+        <Box
+          fontSize={14}
+          display="flex"
+          alignItems="center"
+          position="absolute"
+          bottom={0}
+          right={0}
+          p="6"
+          cursor="pointer"
+        >
+          <Text as="span" pr={2}>
+            Add Item
+          </Text>
+          <AddIcon />
+        </Box>
       </Box>
-      {goals.map(({goal}, idx) => {
-        return <IndividualHabit key={idx} goal={goal} />
-      })}
-      <AchievedHabits />
-    </Box>
+      <Box
+        flexGrow={1}
+        maxW="420"
+        minW="420"
+        height="600"
+        borderColor="gray.500"
+        boxShadow="md"
+        bg={primaryBgColor}
+        p="6"
+        m="3"
+        borderTop="4px"
+        borderTopColor="green.300"
+      >
+        <Text pb="1" as="span" fontWeight={500} fontSize="18px">
+          Completed
+        </Text>
+      </Box>
+      <Box
+        flexGrow={1}
+        maxW="420"
+        minW="420"
+        height="600"
+        borderColor="gray.500"
+        boxShadow="md"
+        bg={primaryBgColor}
+        p="6"
+        m="3"
+        borderTop="4px"
+        borderTopColor="green.300"
+        fontSize="18px"
+      >
+        <Text pb="1" as="span" fontWeight={500}>
+          To Continue
+        </Text>
+      </Box>
+    </Flex>
   )
 }
 
--- client/components/IndividualHabit.jsx
+++ client/components/IndividualHabit.jsx
@@ -1,31 +1,24 @@
 import React from 'react'
-import {Box, Button, Flex, Text, Checkbox } from '@chakra-ui/react'
-import { CloseIcon } from '@chakra-ui/icons'
+import {Box, Flex, Text, Checkbox, useColorModeValue } from '@chakra-ui/react'
 
 const IndividualHabit = (props) => {
+  const primaryBgColor = useColorModeValue('gray.200', 'gray.700')
   return (
-  <Flex alignItems="center" mt="2">
-        <Box
-          height="var(--chakra-sizes-10)"
-          borderRadius="5"
-          bg="green.300"
-          color="white"
-          display="flex"
-          alignItems="center"
-          justifyContent="space-between"
-          width="100%"
-        >
-          <Text pl="3">{props.goal}</Text>
-          <Checkbox pr="3" colorScheme="green" defaultUnchecked />
-        </Box>
-        <Button
-          colorScheme="red"
-          alignItems="center"
-          ml="2"
-          height="var(--chakra-sizes-10)"
-        >
-          <CloseIcon w={3} h={3} />
-        </Button>
+    <Flex alignItems="center" mt="4" mb="4" bg={primaryBgColor} borderRadius="2">
+      <Box
+        height="var(--chakra-sizes-10)"
+        display="flex"
+        alignItems="center"
+        justifyContent="flex-start"
+        width="100%"
+        p='3'
+        fontSize='16'
+      >
+        <Checkbox pr="3" colorScheme="green" border='gray' defaultUnchecked />
+
+        <Text pl="3">{props.goal}</Text>
+      </Box>
+      <Text p="3" whiteSpace="nowrap">7/28 Days</Text>
     </Flex>
   )
 }
