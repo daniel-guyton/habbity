dc824cb323a5dcd701634e6fabd9a9d339b48750
eva-van-seventer <van7ter.eva@icloud.com>
2022-05-07 11:31:26 +1200

    change rootUrl to heroku domain

--- client/apis/apiClient.js
+++ client/apis/apiClient.js
@@ -1,6 +1,8 @@
 import request from 'superagent'
 
-const rootUrl = '/api/v1'
+// => switch from local api to heroku url before deploying to heroku
+const rootUrl = 'https://habbity.herokuapp.com/api/v1'
+// const rootUrl = '/api/v1'
 
 //*   HABITS
 //* ==========
