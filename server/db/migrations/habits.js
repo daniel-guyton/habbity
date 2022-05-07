823d2768fac7acbcec0670baa59ea32a3d4f14c1
eva-van-seventer <van7ter.eva@icloud.com>
2022-05-07 12:25:14 +1200

    push db

--- server/db/migrations/habits.js
+++ server/db/migrations/habits.js
@@ -7,9 +7,9 @@ exports.up = function (knex) {
   return knex.schema.createTable('habits', (table) => {
     table.increments('id')
     table.integer('userID') //.references('users.id') <- this can force habits to have an existing owner so new owners don't inherit misplaced habits. Commented out for development
-    table.timestamp('daysCompleted')
+    table.bigInteger('daysCompleted')
     table.string('goal')
-    table.timestamp('timestamp')
+    table.bigInteger('timestamp')
     table.string('status')
   })
 }
