b10f29632209c35670a42865da3c1dedc1d4b84d
eva-van-seventer <van7ter.eva@icloud.com>
2022-05-07 12:13:39 +1200

    heroku is fun

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-// if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-//   const envConfig = require('dotenv').config()
-//   if (envConfig.error) throw envConfig.error
-// }
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  const envConfig = require('dotenv').config()
+  if (envConfig.error) throw envConfig.error
+}
 
 const express = require('express')
 const path = require('path')
