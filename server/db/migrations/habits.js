36b66d070c84ff4d7c7130ca7a71314b5325ffd0
Chen Yao <c.yao.0413@gmail.com>
2022-05-09 11:33:42 +1200

    change habbit table column type

--- server/db/migrations/habits.js
+++ server/db/migrations/habits.js
@@ -6,7 +6,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('habits', (table) => {
     table.increments('id')
-    table.integer('userID') //.references('users.id') <- this can force habits to have an existing owner so new owners don't inherit misplaced habits. Commented out for development
+    table.string('userID') //.references('users.id') <- this can force habits to have an existing owner so new owners don't inherit misplaced habits. Commented out for development
     table.integer('daysCompleted')
     table.string('goal')
     table.integer('timestamp')
