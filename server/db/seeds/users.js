e5522b184200a04bb1a93e3f3ca1be9a4d605e02
eva-van-seventer <van7ter.eva@icloud.com>
2022-05-09 08:37:22 +1200

    more db deployment

--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -8,9 +8,7 @@
     { username: 'bob', email: 'socks@yahoo.com', auth0: '', points: 420 },
     { username: 'bobby', email: 'fish@yahoo.com', auth0: '', points: 1337 },
     { username: 'bobina', email: 'green@gmail.com', auth0: '', points: 80085 },
-    {
-      username: 'bobtina', email: 'slartibartfast@hotmail.co.uk', auth0: '', points: 69,
-    },
+    { username: 'bobtina', email: 'slartibartfast@hotmail.co.uk', auth0: '', points: 69,},
   ])
 }
 
