a2dc187beaaba1f055d09c6f84f960bc98108125
Chen Yao <c.yao.0413@gmail.com>
2022-05-09 23:32:42 +1200

    WIP add calculation to display more gif based on points

--- client/components/Badges.jsx
+++ client/components/Badges.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-import { Box, Heading, Text, Badge, Divider, Center, Image } from '@chakra-ui/react'
+import { Box, Heading, Text, Badge, Divider, Center, Flex, Spacer } from '@chakra-ui/react'
 import { getBadge, getUserByAuth0Id } from '../apis/apiClient'
 
 
@@ -9,7 +9,8 @@ const Badges = () => {
   const user = useSelector(state => state.user)
   const [userInfo, setUserInfo] = useState(null)
   const [badges, setBadges] = useState([])
-  const [getBadgeSuccess, setGetBadgeSuccess] = useState(false)
+  const [getBadgeSuccess, setGetBadgeSuccess] = useState(0)
+  const [newBadgeToGet, setNewBadgeToGet] = useState(0)
 
   useEffect(() => {
     if (user.auth0Id !== '' && userInfo === null) {
@@ -17,20 +18,38 @@ const Badges = () => {
         .then((res) => {
           setUserInfo(res)
         })
-        .then(() => {
-          setBadges(userInfo.badges.split(','))
-        })
-        .catch(err => console.log(err))
+        .catch(err => console.log('getUserByAuth0Id', err.message))
       }
-    if (user.auth0Id !== '' && !getBadgeSuccess) {
-      getBadge(user)
-        .then((res) => {
-          setBadges([...badges, res.embed_url])
-        })
-        .then(setGetBadgeSuccess(true))
-        .catch(err => console.log(err.message))
+    }, [user])
+
+  useEffect(() => {
+    userInfo !== null && setBadges(userInfo['badges'].split(','))
+    userInfo !== null && calculatePoints(userInfo.points)
+  }, [userInfo])
+
+  useEffect(() => {
+    if(newBadgeToGet > 0 && getBadgeSuccess === 0) {
+      let returnedArray = []
+      for (let i = 0; i < newBadgeToGet; i ++) {
+        if (getBadgeSuccess < newBadgeToGet) {
+          getBadge(user)
+            .then((res) => {
+              console.log(res.embed_url)
+              returnedArray.push(res.embed_url)
+            })
+            .then(setGetBadgeSuccess(getBadgeSuccess + 1))
+            .catch(err => console.log('getBadge', err.message))
+          }
+        }
+      setBadges([...badges, returnedArray])
     }
-    }, [user, userInfo])
+  }, [newBadgeToGet])
+
+  const calculatePoints = (num) => {
+    const badgeToBeAwarded = Math.round(Math.floor(num/2))
+    const badgeAwarded = badges.length
+    setNewBadgeToGet(badgeToBeAwarded - badgeAwarded)
+  }
   
   const badgeStyle = {
     borderRadius: '5px',
@@ -48,6 +67,8 @@ const Badges = () => {
     height: '100%',
     width: '100%'
   }
+
+  console.log(badges)
 
   return (
     <Box w='100%' p={4} color='teal.500'>
@@ -68,18 +89,19 @@ const Badges = () => {
       <Center height='50px'>
         <Divider orientation='vertical' />
       </Center>
-      {
-        badges.map((badge, index) => {
-          if(getBadgeSuccess) {
-            return (
-            <Box key={index} maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' style={giphyStyle}>
-              <iframe src={badge} alt='badge GIF' style={iframeStyle} />
-            </Box>
-            )
-
-          }
-        })
-      }
+      <Flex style={{flexWrap: 'wrap'}}>
+        {badges.length !== null &&
+          badges.map((badge, index) => {
+              return (
+                <>
+                  <Box key={index} maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' style={giphyStyle}>
+                    <iframe src={badge} alt='badge GIF' style={iframeStyle} />
+                  </Box>
+                  <Spacer />
+                </>
+              )
+        })}
+      </Flex>
     </Box>
   )
 }
