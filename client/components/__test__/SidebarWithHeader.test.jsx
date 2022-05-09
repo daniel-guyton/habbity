2c8c0d32a34eb131b76fa74c9041c9fdbef93147
Daniel <daniel.j.guyton@gmail.com>
2022-05-09 22:09:28 +1200

    Add apiClient tests

---
+++ client/components/__test__/SidebarWithHeader.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import SidebarWithHeader from '../SidebarWithHeader'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+describe('<SidebarWithHeader', () => {
+  const state = {
+    user: {
+      token: ''
+    }
+  }
+  const store = {
+    getState: () => state,
+    subscribe: jest.fn()
+  }
+  it('checks if the sidebar links are being rendered', () => {
+    render(
+      <Provider store={store}>
+        <Router>
+          <SidebarWithHeader />
+        </Router>
+      </Provider>
+    )
+    const navItems = screen.getAllByRole('group')
+
+    expect(navItems[0].textContent).toBe("Habits")
+    expect(navItems[1].textContent).toBe('Stats')
+    expect(navItems[2].textContent).toBe('Badges')
+  })
+})
