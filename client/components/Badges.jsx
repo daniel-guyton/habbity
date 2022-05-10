fcbd0c9d607b1b5a2c00f7f1110db17f8f379e58
Daniel <daniel.j.guyton@gmail.com>
2022-05-10 17:03:27 +1200

    add toast and use prettier format

--- client/components/Badges.jsx
+++ client/components/Badges.jsx
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-import { Box, Heading, Text, Badge, Divider, Center, Flex, Spacer } from '@chakra-ui/react'
+import {
+  Box,
+  Heading,
+  Text,
+  Badge,
+  Divider,
+  Center,
+  Flex,
+  Spacer,
+} from '@chakra-ui/react'
 import { getBadge, getUserByAuth0Id } from '../apis/apiClient'
 
-
 const Badges = () => {
-
-  const token = useSelector(state => state.user.token)
-  const user = useSelector(state => state.user)
+  const token = useSelector((state) => state.user.token)
+  const user = useSelector((state) => state.user)
   const [userInfo, setUserInfo] = useState(null)
   const [badges, setBadges] = useState([])
   const [getBadgeSuccess, setGetBadgeSuccess] = useState(0)
@@ -20,88 +27,96 @@ const Badges = () => {
           console.log(res)
           setUserInfo(res)
         })
-        .catch(err => console.log('getUserByAuth0Id', err.message))
-      }
-    }, [token])
+        .catch((err) => console.log('getUserByAuth0Id', err.message))
+    }
+  }, [token])
 
   useEffect(() => {
     console.log(userInfo)
     userInfo?.badges.length > 0 && setBadges(userInfo.badges.split(','))
   }, [userInfo?.badges])
-  
+
   useEffect(() => {
     userInfo !== null && calculatePoints(userInfo.points)
   }, [badges])
 
   useEffect(() => {
-    if(newBadgeToGet > 0) {
+    if (newBadgeToGet > 0) {
       if (getBadgeSuccess <= newBadgeToGet) {
         getBadge(user)
           .then((res) => {
             setBadges([...badges, res.embed_url])
             setGetBadgeSuccess(getBadgeSuccess + 1)
           })
-          .catch(err => console.log('getBadge', err.message))
+          .catch((err) => console.log('getBadge', err.message))
       }
     }
   }, [newBadgeToGet, getBadgeSuccess])
 
   const calculatePoints = (num) => {
-    const badgeToBeAwarded = Math.round(Math.floor(num/2))
+    const badgeToBeAwarded = Math.round(Math.floor(num / 2))
     const badgeAwarded = badges.length
     setNewBadgeToGet(badgeToBeAwarded - badgeAwarded)
   }
-  
+
   const badgeStyle = {
     borderRadius: '5px',
     padding: '5px 10px 5px 10px',
     marginLeft: '20px',
-    textAlign: 'center'
+    textAlign: 'center',
   }
   const giphyStyle = {
     width: '300px',
     height: '300px',
-    backgroundColor: 'transparent'
+    backgroundColor: 'transparent',
   }
 
   const iframeStyle = {
     height: '100%',
-    width: '100%'
+    width: '100%',
   }
 
   console.log(badges)
 
   return (
-    <Box w='100%' p={4} color='teal.500'>
-      <Heading as='h3' size='lg'>
+    <Box w="100%" p={4} color="teal.500">
+      <Heading as="h3" size="lg">
         Welcome {user.name}!
-        { userInfo !== null ? 
-          (
-          <Badge ml='1' fontSize='0.5em' colorScheme='teal' style={badgeStyle}>
+        {userInfo !== null ? (
+          <Badge ml="1" fontSize="0.5em" colorScheme="teal" style={badgeStyle}>
             {userInfo.points} xp
           </Badge>
-          ) : null
-        }
+        ) : null}
       </Heading>
-      <Center height='50px'>
-        <Divider orientation='vertical' />
+      <Center height="50px">
+        <Divider orientation="vertical" />
       </Center>
-      <Text fontSize='3xl'>Here are some treats as a return for being a good habbitor!</Text>
-      <Center height='50px'>
-        <Divider orientation='vertical' />
+      <Text fontSize="3xl">
+        Here are some treats as a return for being a good habbitor!
+      </Text>
+      <Center height="50px">
+        <Divider orientation="vertical" />
       </Center>
-      <Flex style={{flexWrap: 'wrap'}}>
-        {badges && badges.length !== 0 &&
+      <Flex style={{ flexWrap: 'wrap' }}>
+        {badges &&
+          badges.length !== 0 &&
           badges.map((badge, index) => {
-              return (
-                <>
-                  <Box key={index} maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' style={giphyStyle}>
-                    <iframe src={badge} alt='badge GIF' style={iframeStyle} />
-                  </Box>
-                  <Spacer />
-                </>
-              )
-        })}
+            return (
+              <>
+                <Box
+                  key={index}
+                  maxW="sm"
+                  borderWidth="1px"
+                  borderRadius="lg"
+                  overflow="hidden"
+                  style={giphyStyle}
+                >
+                  <iframe src={badge} alt="badge GIF" style={iframeStyle} />
+                </Box>
+                <Spacer />
+              </>
+            )
+          })}
       </Flex>
     </Box>
   )
--- client/components/XP.jsx
+++ client/components/XP.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-import { Tag} from '@chakra-ui/react'
-import { useSelector } from "react-redux";
+import React from 'react'
+import { Tag } from '@chakra-ui/react'
+import { useSelector } from 'react-redux'
 const XP = () => {
-
-  const xp = useSelector(state => state.profile.points) 
+  const xp = useSelector((state) => state.profile.points)
   return (
-      <Tag fontSize='11px'size="sm" key="sm" colorScheme="green" variant="subtle">
-        Points: {xp}
-      </Tag>
+    <Tag
+      fontSize="11px"
+      size="sm"
+      key="sm"
+      colorScheme="green"
+      variant="subtle"
+    >
+      Points: {xp}
+    </Tag>
   )
 }
 
